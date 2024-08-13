0a90b81931ffc0eaeac32e82c5c6477a63d57349
Anurag Vadhyar <92390229+anuragvadhyar@users.noreply.github.com>
2024-08-14 00:23:21 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 from pyspark.sql import functions as F
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
-
+from Stop import get_stopwords
 def preprocessing(data):
     data = data.select(col("_id"),col("text").alias("original_text"),explode(split(data.text, "t_end")).alias("text"), col("score"),col("source"))
     data = data.na.replace('', None)
@@ -16,8 +16,7 @@ def preprocessing(data):
     data = data.withColumn('text', F.regexp_replace('text', "[^ 'a-zA-Z0-9]", ''))
     
 
-    stopwords = ["ourselves", "her", "between", "yourself", "but", "again", "there", "about", "once", "during", "out", "very", "having", "with", "they", "own", "an", "be", "some", "for", "do", "its", "yours", "such", "into", "of", "most", "itself", "other", "off", "is", "am", "or", "who", "as", "from", "him", "each", "the", "themselves", "until", "below", "are", "we", "these", "your", "his", "through", "don", "nor", "me", "were", "her", "more", "himself", "this", "down", "should", "our", "their", "while", "above", "both", "up", "to", "ours", "had", "she", "all", "no", "when", "at", "any", "before", "them", "same", "and", "been", "have", "in", "will", "on", "does", "yourselves", "then", "that", "because", "what", "over", "why", "so", "can", "did", "not", "now", "under", "he", "you", "herself", "has", "just", "where", "too", "only", "myself", "which", "those", "i", "after", "few", "whom", "being", "if", "theirs", "my", "against", "a", "by", "doing", "it", "how", "further", "was", "here", "than"]
-    
+    stopwords=get_stopwords()    
     for stopword in stopwords:
         data = data.withColumn('text', F.regexp_replace('text', ' '+stopword+' ' , ' '))
     data = data.withColumn('text', F.regexp_replace('text', ' +', ' '))
